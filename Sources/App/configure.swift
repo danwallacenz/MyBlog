f972b55f429020d2bc5fce5883de38333fb542b6
Dan Wallace <daniel.wallace.nz@gmail.com>
2020-07-23 06:41:17 +1200

    setup

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,15 @@
 import Vapor
+import Leaf
 
 // configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+    // register Leaf
+    app.views.use(.leaf)
+    app.leaf.cache.isEnabled = app.environment.isRelease
+    
     // register routes
     try routes(app)
 }
