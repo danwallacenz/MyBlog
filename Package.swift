b0d687e51a6d2a1efb5e01931fee9a91d1126511
Dan Wallace <daniel.wallace.nz@gmail.com>
2020-07-23 23:46:52 +1200

    fluent migration and seed db set up

--- Package.swift
+++ Package.swift
@@ -10,6 +10,8 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0-rc"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0-rc"),
     ],
     targets: [
         .target(
@@ -17,6 +19,8 @@ let package = Package(
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "Leaf", package: "leaf"),
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
             ],
             swiftSettings: [
                 // Enable better optimizations when building in Release configuration. Despite the use of
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,5 +1,7 @@
 import Vapor
 import Leaf
+import Fluent
+import FluentSQLiteDriver
 
 // configures your application
 public func configure(_ app: Application) throws {
@@ -10,15 +12,26 @@ public func configure(_ app: Application) throws {
     app.views.use(.leaf)
     app.leaf.cache.isEnabled = app.environment.isRelease
     
+    // Fluent
+    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    
     // register routes
-//    try routes(app)
-//    let router = FrontendRouter()
-//    try router.boot(routes: app.routes)
-    let routers: [RouteCollection] = [
-        FrontendRouter(),
-        BlogRouter(),
-    ]
-    for router in routers {
-        try router.boot(routes: app.routes)
-    }
+//    let routers: [RouteCollection] = [
+//        FrontendRouter(),
+//        BlogRouter(),
+//    ]
+//    for router in routers {
+//        try router.boot(routes: app.routes)
+//    }
+    
+    let modules: [Module] = [
+            FrontendModule(),
+            BlogModule(),
+        ]
+
+        for module in modules {
+            try module.configure(app)
+        }
+    
+
 }
